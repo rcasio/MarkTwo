222b9500288dfab517cc8b1ffa624b0696ce4226
DESKTOP-00H08F0\rcasi <rcasio@naver.com>
2018-02-24 16:43:55 +0900

    코드 정리 GenerateCSharpCode.cs

--- MarkTwo/GenerateCSharpCode.cs
+++ MarkTwo/GenerateCSharpCode.cs
@@ -298,38 +298,6 @@ namespace MarkTwo
                 }
             }
 
-            //foreach (var item in dicPR)
-            //{
-            //    string type = ChangeType(item.Value.Type); // 타입을 CSharp에 맞게 변화시킨다.
-
-            //    if (type.Equals("struct")) // 구조체 시작이라면
-            //    {
-            //        isStruct = true; // 구조체 안쪽에 위치함을 인식시킨다.
-            //        tableTagList.WriteLine("");
-            //        tableTagList.WriteLine("            public " + type + " " + item.Value.Key + " // " + item.Value.Comment); // 구조체 정의를 시작한다.
-            //        tableTagList.WriteLine("            {");
-            //    }
-            //    else if (type.Equals("}")) // 구조체 종료
-            //    {
-            //        isStruct = false; // 구조체 밖에 존재함을 알린다.
-            //        tableTagList.WriteLine("            " + type); // 구조체 정의를 시작한다.
-            //        tableTagList.WriteLine("");
-            //    }
-            //    else
-            //    {
-            //        if (isStruct) // 구조체 안에 들어가 있다면 Tab으로 한번 더 띄운다.
-            //        {
-            //            tableTagList.WriteLine("                public const " + type + " " + item.Value.Key + " = " +
-            //            ChangeValue(type, item.Value.Value) + " // " + item.Value.Comment + " ( Num : " + item.Value.Num + " )");
-            //        }
-            //        else // 구조체에 들어가 있지 않다면
-            //        {
-            //            tableTagList.WriteLine("            public const " + type + " " + item.Value.Key + " = " +
-            //            ChangeValue(type, item.Value.Value) + " // " + item.Value.Comment + " ( Num : " + item.Value.Num + " )");
-            //        }
-            //    }
-            //}
-
             tableTagList.WriteLine("        }");
             tableTagList.WriteLine("");
 
@@ -366,16 +334,7 @@ namespace MarkTwo
                             }
 
                             tableTagList.WriteLine("                " + tagType + ",");
-
-                            //// 마지막에는 ","를 넣지 않는다.
-                            //if (i.Equals(fieldData.Value.contents.Count - 1))
-                            //{
-                            //    tableTagList.WriteLine("                " + tagType);
-                            //}
-                            //else
-                            //{
-                            //    tableTagList.WriteLine("                " + tagType + ",");
-                            //}
+                            
                         }
                     }
 
@@ -388,208 +347,12 @@ namespace MarkTwo
                     }
                 }
             }
-
-            /****** const string 으로 만드는 태그
-            foreach (KeyValuePair<string, Tag> item in dicTag)
-            {
-                m_TableTagList.WriteLine("            public class " + item.Key);
-                m_TableTagList.WriteLine("            {");
-                
-                foreach (string tagCom in item.Value.TagCompoments)
-                {
-                    m_TableTagList.WriteLine("                public const string " + tagCom + " = \"" + tagCom +"\";");
-                }
-
-                m_TableTagList.WriteLine("            }");
-                m_TableTagList.WriteLine("");
-            }
-            */
-
+            
             tableTagList.WriteLine("        }");
             tableTagList.WriteLine("    }");
             tableTagList.WriteLine("}"); // TagManager NameSpace를 종료한다.
         }
-
-        // 테이블 시트 정보를 참조하여 테이블 변환용 코드를 만든다.
-        public void Write(TableDataS tableData)
-        {
-            // 각각의 리스트를 할당하도록 합니다.
-            tableConverter.WriteLine(AddString("      Table.Data_", tableData.name, " = new Dictionary<int, ", tableData.name, ">(", tableData.totalRowCountDeleteComment.ToString(),");"));
-            tableConverter.WriteLine("");
-
-            // Table 클래스를 이어서 만든다.
-            // 클래스의 행을 담도록 한다.
-            tableConverter.WriteLine(AddString("      for ( int i = 0; i < ", tableData.totalRowCountDeleteComment.ToString(), " ; i++ )"));
-            tableConverter.WriteLine("      {");
-
-            // 리스트에 담을 클래스를 생성한다.(리스트는 참조 형식이라 각각의 클래스가 존재해야 한다.)
-            tableConverter.WriteLine(AddString("          row_", tableData.name, " = new ", tableData.name, "();"));
-            tableConverter.WriteLine("");
-
-            // 파일을 읽도록 한다.
-            for (int i = 0; i < tableData.fieldNameList.Count; i++)
-            {
-                string readBinaryFunction = null;
-                string fieldName = tableData.fieldNameList[i];
-                string fieldType = tableData.fieldDataTypeCSharp[i];
-
-                if (fieldType.Equals("bool")) readBinaryFunction = "m_TableDataBinary.ReadBoolean();";
-                else if (fieldType.Equals("byte")) readBinaryFunction = "m_TableDataBinary.ReadByte();";
-                else if (fieldType.Equals("short")) readBinaryFunction = "m_TableDataBinary.ReadInt16();";
-                else if (fieldType.Equals("int")) readBinaryFunction = "m_TableDataBinary.ReadInt32();";
-                else if (fieldType.Equals("float")) readBinaryFunction = "m_TableDataBinary.ReadSingle();";
-                else if (fieldType.Equals("double")) readBinaryFunction = "m_TableDataBinary.ReadDouble();";
-                else if (fieldType.Equals("long")) readBinaryFunction = "m_TableDataBinary.ReadInt64();";
-                else if (fieldType.Equals("string")) readBinaryFunction = "m_TableDataBinary.ReadString();";
-
-                tableConverter.WriteLine(AddString("          row_", tableData.name, ".", fieldName, " = ", readBinaryFunction));
-
-                // ******* 다국어 Tag를 위한 Multilingual 필드 추출
-                if (tableData.name.Equals("Multilingual")  // 만약 Multilingual 테이블 이라면 현재 지원하지 않은 언어일 경우 주석처리한다.
-                    && !fieldName.Equals("Num")) // 그리고 필드가 Num이 아닐 경우
-                {
-                    multilingual.Add(fieldName); // 다국어 리스트를 추가한다.
-                }
-
-                ////****** 다국어 부분 코드 작성
-                //if (tableData.m_Name.Equals("Multilingual")  // 만약 Multilingual 테이블 이라면 현재 지원하지 않은 언어일 경우 주석처리한다.
-                //    && !fieldName.Equals("Num")) // 그리고 필드가 Num이 아닐 경우
-                //{
-                //    multilingual.Add(fieldName); // 다국어 리스트를 추가한다.
-
-                //    // 모든 언어를 불러올지 아니면 특정 언어만 불러 올지를 결정한다.
-                //    m_TableConverter.WriteLine(AddString("          if (setLanugage.Equals(null))")); // 언어 설정이 null이라면
-                //    m_TableConverter.WriteLine(AddString("          {"));
-                //    m_TableConverter.WriteLine(AddString("          row_", tableData.m_Name, ".", fieldName, " = ", readBinaryFunction)); // 모든 언어를 불러온다.
-                //    m_TableConverter.WriteLine(AddString("          }"));
-                //    m_TableConverter.WriteLine(AddString("          else"));
-                //    m_TableConverter.WriteLine(AddString("          {"));
-                //    m_TableConverter.WriteLine(AddString("              if (setLanugage.Equals( \"", fieldName, "\")) row_", tableData.m_Name, ".", fieldName, " = ", readBinaryFunction));
-                //    m_TableConverter.WriteLine(AddString("              else m_TableDataBinary.ReadString();"));
-                //    m_TableConverter.WriteLine(AddString("          {"));
-                //}
-                //else // 일반 테이블 코드 작성
-                //{
-
-                //}
-            }
-
-            // 리스트에 클래스를 참조값을 담는다.
-            tableConverter.WriteLine("");
-            tableConverter.WriteLine(AddString("          Table.Data_", tableData.name, ".Add(row_",tableData.name,".Num, row_", tableData.name, ");"));
-            tableConverter.WriteLine("");
-
-            tableConverter.WriteLine("      }");
-            tableConverter.WriteLine("");
-        }
-
-        // 테이블 시트를 참조하여 시트 클래스를 만든다.
-        public void WriteCode_TableClassList(TableDataS tableData)
-        {   
-            // 테이블 단위의 개별 클래스를 만든다.
-            tableClassList.WriteLine("  [System.Serializable] "); // 에디터에서 확인할 수 있게 [System.Serializable]을 하도록 한다.
-            tableClassList.WriteLine("  public class " + tableData.name);
-            tableClassList.WriteLine("  {");
-
-            int i = 0;
-
-            foreach (string fieldName in tableData.fieldNameList)
-            {
-                string fieldType = tableData.fieldDataTypeCSharp[i];
-
-                tableClassList.WriteLine(AddString("    public ", fieldType, " ", fieldName, ";"));
-
-                i++;
-            }
-
-            tableClassList.WriteLine("  }");
-        }
-
-        // 테이블 시트를 참조하여 Multilingual 및 PR 클래스를 만든다.
-        public void WriteCode_TableTagMultilingualPR(TableDataS tableData)
-        {
-            tableTagList.WriteLine("// 테이블 태그를 사용하기 위한 클래스입니다."); // 사용확인을 위한 주석
-            tableTagList.WriteLine("// 툴에의해 자동으로 생성됩니다.");
-            tableTagList.WriteLine("// 2017년 09월 26일 고성규");
-            tableTagList.WriteLine("");
-            tableTagList.WriteLine("namespace TagManager"); // 태그를 관리하기 위한 네임스페이스를 만들도록 한다.
-            tableTagList.WriteLine("{");
-            tableTagList.WriteLine("    public class Table"); // 테이블 클래스를 만든다.
-            tableTagList.WriteLine("    {");
-
-            //****** 다국어 관련 태그
-            tableTagList.WriteLine("        // 다국어 대응 열거자 입니다. Multilingual 테이블을 기반으로 작성됩니다.");
-            tableTagList.WriteLine("        public enum Multilingual"); // 다국어 열거자를 만든다.
-            tableTagList.WriteLine("        {");
-
-            // 다국어에 사용하는 언어를 기입한다.
-            for (int i = 0; i < multilingual.Count; i++)
-            {
-                // 마지막 라인일 경우
-                if (i.Equals(multilingual.Count - 1))
-                {
-                    tableTagList.WriteLine("            " + multilingual[i]); // ","를 빼도록 한다.
-                }
-                else // 마지막 라인일 경우
-                {
-                    tableTagList.WriteLine("            " + multilingual[i] + ",");
-                }
-            }
-
-            tableTagList.WriteLine("        };");
-            tableTagList.WriteLine("");
-
-            //****** PR 관련 태그
-            // PR 클래스를 만든다.
-            // PR 테이블은 변수 대응하기 위한 테이블이다.
-            if (tableData.name.Equals("PR"))
-            {
-                tableTagList.WriteLine("        // PR 테이블 대응 클래스 입니다.");
-                tableTagList.WriteLine("        // PR 테이블은 개별 변수를 정의하기 위한 테이블입니다.");
-                tableTagList.WriteLine("        public class PR");
-                tableTagList.WriteLine("        {");
-
-                bool isStruct = false;
-
-                foreach (var item in dicPR)
-                {
-                    string type = ChangeType(item.Value.Type); // 타입을 CSharp에 맞게 변화시킨다.
-
-                    if (type.Equals("struct")) // 구조체 시작이라면
-                    {
-                        isStruct = true; // 구조체 안쪽에 위치함을 인식시킨다.
-                        tableTagList.WriteLine("");
-                        tableTagList.WriteLine("            public " + type + " " + item.Value.Key + " // " + item.Value.Comment); // 구조체 정의를 시작한다.
-                        tableTagList.WriteLine("            {");
-                    }
-                    else if (type.Equals("}")) // 구조체 종료
-                    {
-                        isStruct = false; // 구조체 밖에 존재함을 알린다.
-                        tableTagList.WriteLine("            " + type); // 구조체 정의를 시작한다.
-                        tableTagList.WriteLine("");
-                    }
-                    else
-                    {
-                        if (isStruct) // 구조체 안에 들어가 있다면 Tab으로 한번 더 띄운다.
-                        {
-                            tableTagList.WriteLine("                public const " + type + " " + item.Value.Key + " = " +
-                            ChangeValue(type, item.Value.Value) + " // " + item.Value.Comment + " ( Num : " + item.Value.Num + " )");
-                        }
-                        else // 구조체에 들어가 있지 않다면
-                        {
-                            tableTagList.WriteLine("            public const " + type + " " + item.Value.Key + " = " +
-                            ChangeValue(type, item.Value.Value) + " // " + item.Value.Comment + " ( Num : " + item.Value.Num + " )");
-                        }
-                    }
-                }
-
-                tableTagList.WriteLine("        }");
-                tableTagList.WriteLine("");
-
-                // 이 밑으로 WriteCode_TableTag 함수를 사용하여 Tag 테이블이 추가된다.
-            }
-        }
-
+        
         public string ChangeValue(string type, string value)
         {
             string tValue = value;
@@ -633,78 +396,7 @@ namespace MarkTwo
 
             return rtype;
         }
-
-        // Tag 테이블을 참조하여 추가 Tag 클래스를 만든다.
-        public void WriteCode_TableTag(TableDataS tableData)
-        {
-            // 테이블 Tag로 구성되도록 한다.
-            tableTagList.WriteLine("        public class Tag");
-            tableTagList.WriteLine("        {");
-
-            int icount = 0;
-            foreach (KeyValuePair<string, Tag> item in dicTag) // 태그 enum를 생성한다.
-            {
-                icount++;
-                tableTagList.WriteLine("            public enum " + item.Key);
-                tableTagList.WriteLine("            {");
-
-                // 개별 Tag enum을 생성한다.
-                for (int i = 0; i < item.Value.TagCompoments.Count; i++)
-                {
-                    string tagCom = item.Value.TagCompoments[i];
-
-                    // Type에서 Char(50)과 VarChar(50)을 Char50, VarChar50으로 변경한다.
-                    if (item.Key.Equals("Type"))
-                    {
-                        if (tagCom.Contains("Char")) // Char, VarChar이 포함되었을 경우
-                        {
-                            // ( )를 삭제한다(코드에서 인식시키기 위해)
-                            tagCom = tagCom.Replace("(", "");
-                            tagCom = tagCom.Replace(")", "");
-                        }
-                    }
-
-                    // 마지막에는 ","를 넣지 않는다.
-                    if (i.Equals(item.Value.TagCompoments.Count - 1))
-                    {
-                        tableTagList.WriteLine("                " + tagCom);
-                    }
-                    else
-                    {
-                        tableTagList.WriteLine("                " + tagCom + ",");
-                    }
-                }
-                
-                tableTagList.WriteLine("            }");
-
-                // 마지막 Tag Enum혹은 클래스는 줄을 띄우지 않는다.
-                if (!icount.Equals(dicTag.Count))
-                {
-                    tableTagList.WriteLine("");
-                }
-            }
-
-            /****** const string 으로 만드는 태그
-            foreach (KeyValuePair<string, Tag> item in dicTag)
-            {
-                m_TableTagList.WriteLine("            public class " + item.Key);
-                m_TableTagList.WriteLine("            {");
-                
-                foreach (string tagCom in item.Value.TagCompoments)
-                {
-                    m_TableTagList.WriteLine("                public const string " + tagCom + " = \"" + tagCom +"\";");
-                }
-
-                m_TableTagList.WriteLine("            }");
-                m_TableTagList.WriteLine("");
-            }
-            */
-
-            tableTagList.WriteLine("        }");
-            tableTagList.WriteLine("    }");
-            tableTagList.WriteLine("}"); // TagManager NameSpace를 종료한다.
-        }
-
+        
         // 파일 경로를 반환한다.
         private string Create_FilePath_TableConverter() { return Application.StartupPath + "\\" + TABLECONVERTER_FILENAME; }
 
